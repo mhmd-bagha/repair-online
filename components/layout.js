24191fed7e4c3a156e0f799f6a7a542d3c84d9af
mhmddev <mabrahimibagha@gmail.com>
2023-04-15 18:59:10 +0330

    feat: added custom layout

---
+++ components/layout.js
@@ -0,0 +1,7 @@
+const Layout = ({children}) => {
+    return (
+        <div className="px-4 xl:px-80">{children}</div>
+    )
+}
+
+export default Layout
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import 'Styles/modules.css'
+import 'Styles/globals.scss'
+import Layout from "Components/layout";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <Layout>
+          <Component {...pageProps} />
+      </Layout>
+  )
 }
