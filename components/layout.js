2b8f742d46e4c10a2a104f70634151b49c2975e1
mhmddev <mabrahimibagha@gmail.com>
2023-04-15 23:11:12 +0330

    feat: added footer to layout

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,11 @@
+import Footer from "Components/footer";
+
 const Layout = ({children}) => {
     return (
-        <div className="px-4 xl:px-80">{children}</div>
+        <>
+            <div className="px-4 xl:px-80">{children}</div>
+            <Footer/>
+        </>
     )
 }
 
