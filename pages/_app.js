086b4d19c88b314c8c80bc9ef78b1fcf29684f36
mhmddev <mabrahimibagha@gmail.com>
2023-04-29 12:04:47 +0330

    feat: added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
 import 'Styles/modules.css'
 import 'Styles/globals.scss'
 import Layout from "Components/layout";
+import ReactGA from 'react-ga';
 
 export default function App({ Component, pageProps }) {
+    ReactGA.initialize('G-FBS3BR4DX3')
+
   return (
       <Layout>
           <Component {...pageProps} />
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,8 +3,13 @@ import Intro from "Components/home/intro";
 import ServicesOusWork from "Components/home/services-ous-work";
 import ContactUs from "Components/home/contact-us";
 import Seo from "Components/home/seo";
+import ReactGA from 'react-ga';
 
 const Home = () => {
+    const isServer = typeof window === 'undefined';
+    const mainUrl = isServer ? '' : window.location.pathname + window.location.search;
+    ReactGA.pageview(mainUrl)
+
     return (
         <>
             <Menu/>
